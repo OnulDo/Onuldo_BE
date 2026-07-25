a9693cac64b371cb5608aab21501d66751f9f2ec
이용재 <56459713+YONGJAEEE@users.noreply.github.com>
2026-07-25 17:55:36 +0900

    #9-유저가 참여중인 챌린지 조회

--- src/main/java/com/example/onuldo/domain/challenge/repository/ParticipationRepository.java
+++ src/main/java/com/example/onuldo/domain/challenge/repository/ParticipationRepository.java
@@ -1,9 +1,16 @@
 package com.example.onuldo.domain.challenge.repository;
 
 import com.example.onuldo.domain.challenge.entity.Participation;
+import com.example.onuldo.domain.challenge.enums.ParticipationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
 
     boolean existsByUser_IdAndChallenge_Id(Long userId, Long challengeId);
+
+    List<Participation> findAllByUser_IdOrderByIdDesc(Long userId);
+
+    List<Participation> findAllByUser_IdAndStatusOrderByIdDesc(Long userId, ParticipationStatus status);
 }
