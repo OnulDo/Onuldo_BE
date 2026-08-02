270597df8c3b7912aab48977e028019a4eb33be9
lyn0719 <usolnim@naver.com>
2026-08-03 00:39:09 +0900

    #65 파티 대기방 이탈 API 성공 응답 커스텀 메시지 적용

--- src/main/java/com/example/onuldo/domain/party/controller/PartyController.java
+++ src/main/java/com/example/onuldo/domain/party/controller/PartyController.java
@@ -88,7 +88,7 @@ public class PartyController implements PartyControllerDoc {
             @PathVariable
             Long partyId
     ) {
-        return BaseResponse.onSuccess(partyMemberService.leaveParty(partyId, userId));
+        return BaseResponse.onSuccess("파티 이탈에 성공했습니다.", partyMemberService.leaveParty(partyId, userId));
     }
 
     // 준비완료 전환 API는 파티 API 목록(7개)에 명시되어 있지 않았으나 PAR-05, PAR-ERR-03 근거로 추가함 (BE 확인 필요)
