9d629b8da8a81b14103f4f429a3680378d2c75fc
minetoiles <byeol9253@gmail.com>
2026-08-10 15:10:59 +0900

    #168 이메일 검증 정규식 수정

--- src/main/java/com/example/onuldo/domain/auth/dto/request/EmailLoginReqDto.java
+++ src/main/java/com/example/onuldo/domain/auth/dto/request/EmailLoginReqDto.java
@@ -12,7 +12,8 @@ public record EmailLoginReqDto(
         @Schema(example = "onuldo@onuldo.com")
         @NotBlank(message = "이메일은 필수입니다.")
         @Pattern(
-                regexp = "^[a-zA-Z0-9+\\-_\\.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
+                regexp = "^[a-zA-Z0-9+_-]+(\\.[a-zA-Z0-9+_-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?" +
+                        "(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)*\\.[a-zA-Z]{2,}$",
                 message = "올바른 이메일 형식이 아닙니다."
         )
         String email,
