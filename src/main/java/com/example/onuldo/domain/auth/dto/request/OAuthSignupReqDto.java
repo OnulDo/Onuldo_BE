5ceee1ec53da865103963bd31e552d750e2ea4cb
minetoiles <byeol9253@gmail.com>
2026-08-12 22:25:56 +0900

    #222 소셜 회원가입 profileUrl 스키마 example 수정

--- src/main/java/com/example/onuldo/domain/auth/dto/request/OAuthSignupReqDto.java
+++ src/main/java/com/example/onuldo/domain/auth/dto/request/OAuthSignupReqDto.java
@@ -21,7 +21,7 @@ public record OAuthSignupReqDto(
         @NotBlank(message = "닉네임은 필수입니다.")
         String nickname,
 
-        @Schema(example = "https://cdn.onuldo.com/profile/default.png")
+        @Schema(example = "https://onuldo-bucket.s3.ap-northeast-2.amazonaws.com/profile/1.png")
         @NotBlank(message = "프로필 이미지 URL은 필수입니다.")
         String profileImageUrl,
 
