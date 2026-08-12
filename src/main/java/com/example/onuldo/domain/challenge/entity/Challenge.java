f764efcec796bc16f39038dca395b840dfd398c9
im3zero <im3zero@gmail.com>
2026-08-12 21:53:01 +0900

    Revert "챌린지 이미지 URL 컬럼 길이 수정"
    This reverts commit fe93afdf31428d36379e684271976f9c15265529.

--- src/main/java/com/example/onuldo/domain/challenge/entity/Challenge.java
+++ src/main/java/com/example/onuldo/domain/challenge/entity/Challenge.java
@@ -50,13 +50,13 @@ public class Challenge {
     @Column(name = "description", nullable = false, columnDefinition = "TEXT")
     private List<ContentBlockResDto> description;
 
-    @Column(name = "caption_img_url", nullable = false, length = 255)
+    @Column(name = "caption_img_url", nullable = false, length = 500)
     private String captionImgUrl;
 
     @Column(name = "verify_method_content", nullable = false, columnDefinition = "TEXT")
     private String verifyMethodContent;
 
-    @Column(name = "verification_example_photo_url", nullable = false, length = 255)
+    @Column(name = "verification_example_photo_url", nullable = false, length = 500)
     private String verificationExamplePhotoUrl;
 
     @Column(name = "participant_count", nullable = false)
